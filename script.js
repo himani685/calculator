180d47619f270f29bd756495a595b6d0ca785733
himani685 <140187176+himani685@users.noreply.github.com>
2024-02-11 23:32:15 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+function Add()
+ {
+let Num1 = +document.getElementById("number1").value
+let Num2 = +document.getElementById("number2").value
+ //let  result = Num1 +Num2
+  // console.log(Num1 + Num2)
+   let result = Num1 + Num2
+   alert(result)
+  
+ }
+function Sub(){
+  let Num1 = +document.getElementById("number1").value
+  let Num2 = +document.getElementById("number2").value
+   let  result = Num1 -Num2
+     //console.log(Num1 - Num2)
+  alert(result)
+}
+function Mul(){
+  let Num1 = +document.getElementById("number1").value
+  let Num2 = +document.getElementById("number2").value
+   let  result = Num1 *Num2
+    // console.log(Num1 * Num2)
+  alert(result)
+}
+function Div(){
+  let Num1 = +document.getElementById("number1").value
+  let Num2 = +document.getElementById("number2").value
+   let  result = Num1 /Num2
+     //console.log(Num1 / Num2)
+  alert(result)
+}
